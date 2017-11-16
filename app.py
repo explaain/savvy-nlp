a65ed1019a4fb42a7556c55279c7ab4aa1d8bf24
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-11-16 18:47:09 +0000

    fix5

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -42,5 +43,5 @@ def get_results():
 
 if __name__ == '__main__':
   # Bind to PORT if defined, otherwise default to 5000.
-  # port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0')
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
