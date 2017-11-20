2cddeabf9aec31298710aca91d6eded3c059a1b1
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-11-20 01:44:41 +0000

    Now does what the old algorithm did!

--- app.py
+++ app.py
@@ -1,121 +1,21 @@
 #!/usr/bin/env python
 import os
-import re
-from algoliasearch import algoliasearch
 from flask import Flask, jsonify, request
+from parse import parse
 
-client = algoliasearch.Client("I2VKMNNAXI", 'be6155d34789643ea81563fb671ebb85')
-index = client.init_index('Savvy')
 app = Flask(__name__)
 
-results = {
-  'hits': [
-    {
-      'content': {
-        'description': 'This is a card'
-      },
-      'objectID': '123',
-      'teams': [
-        'gqLdFXQ4Z9SAHfjd6IXX'
-      ],
-      'highlight': True
-    }
-  ],
-  'pings': [
-
-  ],
-  'memories': [
-
-  ],
-  'reminders': [
-
-  ]
-}
-
-res = index.search("query string")
-
-def processPageData(organisationID, user, pageData):
-    commonWords = commonWords()
-    emailPhrases = emailPhrases()
-
-    for phrase in emailPhrases:
-        re.sub(phrase, '', pageData)
-
-def compoundSearch(user, pageText):
-    try:
-
-    except ValueError:
-        print("Oops!  This didn't work...")
-
-def commonWords():
-    listOfCommonWords = ['i',
-      'a',
-      'of',
-      'me',
-      'my',
-      'is',
-      'im',
-      'so',
-      'all',
-      'get',
-      'how',
-      'new',
-      'out',
-      'the',
-      'use',
-      'best',
-      'name',
-      'next',
-      'take',
-      'what',
-      'image',
-      'something',
-    ]
-    return listOfCommonWords
-
-def emailPhrases():
-    listOfEmailPhrases = ['Skip to content',
-      'Using',
-      'with screen readers',
-      'Search',
-      'Mail',
-      'COMPOSE',
-      'Labels',
-      'Inbox',
-      'Starred',
-      'Sent Mail',
-      'Drafts',
-      'More',
-      '---------- Forwarded message ----------',
-      'From: ',
-      'Date: ',
-      'Subject: ',
-      'To: ',
-      'Click here to Reply or Forward',
-      'GB',
-      'GB used',
-      'Manage',
-      'Program Policies',
-      'Powered by Google',
-      'Last account activity:',
-      'hour ago',
-      'hours ago',
-      'Details',
-    ]
-    return listOfEmailPhrases
-
+# print(parse.getResults({ 'userID': '123', 'content': 'Some content' }))
 
 @app.route('/parse', methods=['POST'])
 def get_results():
-  results['memories'].append({
-    'content': {
-      'description': request.json['content'][:100]
-    },
-    'objectID': '246',
-    'teams': [
-      'gqLdFXQ4Z9SAHfjd6IXX'
-    ]
-  })
+  print('job')
+  try:
+    results = parse.getResults(request.json)
+  except Exception as e:
+    print('Main error')
+    print(e)
+
   return jsonify({'results': results})
 
 if __name__ == '__main__':
---
+++ parse/search.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import math
+import json
+from algoliasearch import algoliasearch
+
+client = algoliasearch.Client("I2VKMNNAXI", 'be6155d34789643ea81563fb671ebb85')
+index = client.init_index('Savvy')
+
+def search(organisationID, user, query):
+  try:
+    if 'data' in user and 'teams' in user['data']:
+      filters = 'teams: "' + '" OR teams: "'.join(list(map(lambda x: x['team'], user['data']['teams']))) + '"'
+      print(filters)
+    results = index.search(query, {
+      'filters': filters
+    })
+    cards = algoliaToCards(results)
+    return cards
+  except Exception as e:
+    print('------ Error:', e)
+    return { 'error': True }
+  except:
+    print(123)
+
+def compound(organisationID, user, query):
+  try:
+    length = 400
+    limit = 20
+    results = list(map(lambda x:
+      search(organisationID, user, query[length * x:length * (x + 1)]),
+      range(0, min(int(math.ceil(len(query) / length) + 1), limit))
+      ))
+    results = [x for x in results if x != []]
+    cards = []
+    for result in results:
+      cards += result
+    cards = deDup(cards)
+    print(json.dumps(cards, indent=1))
+    return cards
+  except Exception as e:
+    print('------ Error:', e)
+    return { 'error': True }
+  except:
+    print(1234)
+
+def algoliaToCards(results):
+  try:
+    cards = results['hits']
+    for i, card in enumerate(cards):
+      card['content'] = {}
+      if 'description' in card:
+        card['content']['description'] = card['description']
+        del card['description']
+      if 'listItems' in card:
+        card['content']['listItems'] = card['listItems']
+        del card['listItems']
+      if '_highlightResult' in card: del card['_highlightResult']
+      cards[i] = {
+        'card': card
+      }
+    return cards
+  except Exception as e:
+    print('------ Error:', e)
+    return { 'error': True }
+  except:
+    print(12345)
+
+def deDup(array):
+  try:
+    for i, card1 in enumerate(array):
+      for j, card2 in enumerate(array[0:i]):
+        if card2 and card1['card']['objectID'] == card2['card']['objectID']:
+          array[j] = {}
+    array = [x for x in array if x != {}]
+    return array
+  except Exception as e:
+    print('------ Error:', e)
+    return { 'error': True }
+  except:
+    print(123456)
