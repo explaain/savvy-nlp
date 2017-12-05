b6fcfff0426429c7370481a708dc234f5eb4d9cb
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-12-05 16:20:47 +0000

    Auto indexing?

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 import pprint, datetime, sched, time
 from algoliasearch import algoliasearch
 from parse import drives
+import xmljson
 pp = pprint.PrettyPrinter(indent=4)
 
 client = algoliasearch.Client("I2VKMNNAXI", 'a202abb1140bd81265eb38b186d822d3') # This API key was generated specifically for modifying SavvyFiles and SavvyChunks indices
@@ -10,16 +11,18 @@ algoliaChunksIndex = client.init_index('SavvyChunks')
 
 def indexFiles():
   memory = open('memory.txt','r')
-  lastRefresh = datetime.datetime.strptime(memory.read().splitlines()[0], '%Y-%m-%d %H:%M:%S.%f')
-  print(lastRefresh)
-  memory = open('memory.txt','w')
-  memory.write(str(datetime.datetime.now()))
-  files = drives.listfiles('', '', after=lastRefresh)
+  lastRefreshTime = datetime.datetime.strptime(memory.read().splitlines()[0], '%Y-%m-%d %H:%M:%S.%f')
+  print(lastRefreshTime)
+  thisRefreshTime = str(datetime.datetime.now())
+  files = drives.listfiles('', '', after=lastRefreshTime)
+  pp.pprint([f['title'] for f in files])
   pp.pprint(files)
-  # algoliaFilesIndex.add_objects(files)
 
   for f in files:
     indexFile(f['objectID']) # We index them again because they seem to first come in with their original names e.g. "Untitled document"
+
+  memory = open('memory.txt','w') # Happens now so that incomplete indexing doesn't overwrite lastRefreshTime
+  memory.write(thisRefreshTime)
 
 def indexFile(fileID):
   f = drives.getfile('', '', fileID=fileID)
@@ -37,18 +40,45 @@ def indexFileContent(f):
 
   # Create new chunks
   contentArray = drives.getContent('', '', f['objectID'], True)
-  print(contentArray)
-  cards = [{
-    'content': c,
-    'fileID': f['objectID'],
-    'fileTitle': f['title'],
-    'created': f['created'],
-    'modified': f['modified'],
-    'type': 'p',
-    'index': i
-  } for i,c in enumerate(contentArray)]
+  print('contentArray', contentArray)
+  cards = createCardsFromContentArray(contentArray, f)['allCards']
+  # print(cards)
   pp.pprint(cards)
-  algoliaChunksIndex.add_objects(cards)
+
+
+def createCardsFromContentArray(contentArray, f):
+  print('createCardsFromContentArray')
+  print('contentArray', contentArray)
+  cards = []
+  allCards = []
+  for i, chunk in enumerate(contentArray):
+    card = {
+      'content': ' '.join(chunk['content']),
+      'fileID': f['objectID'],
+      'fileTitle': f['title'],
+      'created': f['created'],
+      'modified': f['modified'],
+      'type': 'p',
+      'index': i
+    }
+    if 'chunks' in chunk:
+      subdata = createCardsFromContentArray(chunk['chunks'], f)
+      subcards = subdata['cards']
+      allCards += subdata['allCards']
+      # cards += subcards
+      card['listItems'] = [c['objectID'] for c in subcards]
+      card['listCards'] = [c['content'] for c in subcards]
+    cards.append(card)
+    allCards.append(card)
+  cardIDs = algoliaChunksIndex.add_objects(cards)
+  print(cardIDs['objectIDs'])
+  for i, objectID in enumerate(cardIDs['objectIDs']):
+    cards[i]['objectID'] = objectID
+  return {
+    'cards': cards, # Cards on this level
+    'allCards': allCards # Cards passed up that should continue being passed
+  }
+
 
 
 minsInterval = 10
@@ -62,5 +92,10 @@ s.run()
 
 
 # indexFiles()
-# indexFile('FBNJFgtvI4HkQoiqKPmByPL0BYCrnkAy_iP56qn2QMe8=')
+# indexFile('FqTmNLmwOqrf3tLudMvuJYhRhFgdMPgFk4nWbFGLEnN5jjQSba26Olb_j6G_YZQEm')
 # indexFileContent({'objectID': 'FVNDMMXfVj99RqJMyz1xiFpk63kKA44NqCKEKimaUF1F63QxFJmvnRuuGKN2JyLXY', 'title': 'Policy Tracker for GE2017.com', 'modified': '1499418614', 'created': 'null'})
+
+
+# xmlstring = open('parse/sample.xml').read()
+# print(xmlstring)
+# drives.xmlFindText(xmlstring)
--- parse/drives.py
+++ parse/drives.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-import re, io, pprint, itertools, zipfile
-pp = pprint.PrettyPrinter(indent=4)
+import re, io, pprint, itertools, zipfile, json, untangle
+from collections import OrderedDict
+import xmljson
+from xmljson import parker, Parker
+# bf = BadgerFish(dict_type=OrderedDict)
+from xml.etree.ElementTree import fromstring
+pp = pprint.PrettyPrinter(indent=1)
 
 import kloudless
 kloudless.configure(api_key='q4djKR_UXs8MxmUv8M56WdTVihDK6Z7ci8JnL1qJvC2Xx40T')
@@ -17,13 +22,16 @@ def getContent(organisationID, user, fileID, asArray=False):
       doc = docs[0]
       xmlContent = z.read(doc).decode('utf-8')
       print(xmlContent)
-      reg = re.findall('>([^<>]*)<', xmlContent)
-      contentArray = [r for r in reg if len(r) > 0 and r != '\r']
-      if asArray:
-        return contentArray
-      else:
-        content = ' '.join(contentArray)
-        return content
+      xml = xmlFindText(xmlContent)
+      print('xml', xml)
+      return xml
+      # reg = re.findall('>([^<>]*)<', xmlContent)
+      # contentArray = [r for r in reg if len(r) > 0 and r != '\r']
+      # if asArray:
+      #   return contentArray
+      # else:
+      #   content = ' '.join(contentArray)
+      #   return content
     else:
       return ''
     pass
@@ -90,8 +98,152 @@ def fileToCard(f):
   }
   return card
 
+# def xmlFindText(xmlContent):
+#   obj = json.loads(json.dumps(parker.data(fromstring(xmlContent))).replace('{http://schemas.openxmlformats.org/wordprocessingml/2006/main}',''))
+#   pp.pprint(obj)
+#   textArray = []
+#   ongoingCounter = []
+#   xmlSearchTextInChildren(obj, None, 0, textArray, ongoingCounter)
+#   print('textArray')
+#   pp.pprint(textArray)
+#
+#   # For comparison:
+#   reg = re.findall('>([^<>]*)<', xmlContent)
+#   # print([r for r in reg if len(r) > 0 and r != '\r'])
+
+# def xmlSearchTextInChildren(obj, parentKeyOrItem, level, arrayPassedDown, ongoingCounter):
+#   # print(level)
+#   try:
+#     for keyOrItem in obj:
+#       # print('level:', level, ', keyOrItem:', keyOrItem)
+#
+#       if keyOrItem == '$':
+#         ongoingCounter.append(len(ongoingCounter))
+#         print({level: obj[keyOrItem]})
+#         arrayPassedDown.append({level: obj[keyOrItem]})
+#       if parentKeyOrItem == 'p':
+#         arrayPassedDown = []
+#       nextObj = keyOrItem if isinstance(keyOrItem, dict) or isinstance(keyOrItem, list) else (obj[keyOrItem] if isinstance(obj, dict) else None)
+#       # print('nextObj', nextObj)
+#       if isinstance(nextObj, dict) or isinstance(nextObj, list):
+#         print(ongoingCounter, keyOrItem)
+#         xmlSearchTextInChildren(nextObj, keyOrItem, level + 1, arrayPassedDown, ongoingCounter)
+#       # else:
+#       #   print('stopped')
+#
+#       if parentKeyOrItem == 'p':
+#         textArray = arrayPassedDown
+#         print('textArray1')
+#         pp.pprint(textArray)
+#   except Exception as e:
+#     print(e)
+
+
+def xmlFindText(xmlContent):
+  xml = untangle.parse(xmlContent)
+  textArray = []
+  ongoingCounter = []
+  xmlSearchTextInChildren(xml, 0, textArray, ongoingCounter, { 'parFormatting': False, 'hyperlink': False })
+  textArray = [chunk for chunk in textArray if len(chunk['content'])]
+  print('textArray')
+  pp.pprint(textArray)
+  chunkHierarchy = getChunkHierarchy(textArray)
+  pp.pprint(chunkHierarchy)
+  return chunkHierarchy
+
+def xmlSearchTextInChildren(xml, level, dataPassedDown, ongoingCounter, context):
+  children = xml.__dict__['children'] or xml['children']
+  spacing = ''.join([' ' for l in range(level)])
+  if children is not None:
+    for child in children:
+      child.context = dict(context) # Is NOT a deep copy
+    for i, child in enumerate(children):
+      print(spacing, child)
+      if child._name == 'w_p':
+        chunkData = {
+          'content': [],
+          'ranking': 0
+        }
+      else:
+        chunkData = dataPassedDown
+
+      if 'cdata' in child.__dict__ and child.__dict__['cdata'] and not child.__dict__['cdata'].isspace() and child._name != 'w_instrText':
+        dataPassedDown['content'].append(child.__dict__['cdata'].strip())
+        if child.context['hyperlink']:
+          dataPassedDown['content'][-1] = '[' + dataPassedDown['content'][-1] + '](' + child.context['hyperlink'] + ')'
+      if child._name == 'w_pPr':
+        child.context['parFormatting'] = True
+      if child.context['parFormatting']:
+        dataPassedDown['ranking'] += getRanking(child)
+      if hasattr(child, 'w_instrText') and 'cdata' in child.w_instrText.__dict__ and 'HYPERLINK' in child.w_instrText.__dict__['cdata'] and i < len(children) - 1:
+        p = re.compile('HYPERLINK \"(.*)\"')
+        children[i+1].context['hyperlink'] = p.search(child.w_instrText.__dict__['cdata']).group(1)
+      try:
+        xmlSearchTextInChildren(child, level + 1, chunkData, ongoingCounter, child.context)
+      except Exception as e:
+        print(e)
+      if child._name == 'w_p':
+        dataPassedDown.append(chunkData)
+
+
+def getRanking(el):
+  ranking = 0
+  if el._name == 'w_numPr':
+    ranking = -10
+  if el._name == 'w_sz': # Doesn't appear to work on headings?
+    ranking = int(el._attributes['w:val'])
+  if el._name == 'w_pStyle':
+    print('w_pStyle w_pStyle')
+    style = el._attributes['w:val']
+    styles = {
+      'Heading1': 400,
+      'Heading2': 300,
+      'Heading3': 200,
+      'Heading4': 100,
+      'Heading5': 60,
+      'Heading6': 40
+    }
+    print('style', style)
+    if style in styles:
+      ranking = styles[style]
+  if el._name == 'w_b' and el._attributes['w:val'] == '1':
+    ranking = 10
+  return ranking
+
+def getChunkHierarchy(textArray):
+  print('getChunkHierarchy')
+  print(textArray)
+  chunks = []
+  base = 0
+  for i, chunk in enumerate(textArray):
+    print(chunk['ranking'])
+    if chunk['ranking'] >= textArray[base]['ranking'] and i > 0:
+      print('go!!', base+1, i)
+      if textArray[i-1]['ranking'] == chunk['ranking']:
+        chunks.append(textArray[i-1])
+      else:
+        print(textArray[base+1:i])
+        myChunk = textArray[base]
+        myChunk['chunks'] = getChunkHierarchy(textArray[base+1:i])
+        chunks.append(myChunk)
+      base = i
+    if i == len(textArray)-1:
+      print('end!!', base+1, i)
+      if textArray[i]['ranking'] == textArray[base]['ranking']:
+        chunks.append(textArray[i])
+      else:
+        print(textArray[base+1:])
+        myChunk = textArray[base]
+        myChunk['chunks'] = getChunkHierarchy(textArray[base+1:])
+        chunks.append(myChunk)
+    # if chunk['ranking'] < baseRanking and base == 0:
+    #   base = i
+  # if len(chunks) == 0:
+  #   chunks = textArray
+  return chunks
+
+
+
 # search('', '', 'savvy')
-
-
 # getContent('', '', 'FvsojaQrWWSNvuxbjS_hrw_fEVkEY8ykbb5kJh_cB-C_l-zw9ZnOEXZsq9HIp15dD')
 # getContent('', '', 'FyjC21jL7Hy3j55_Ek-d5U_oSk3pt4e7zjAq30HGyD7MoWBYhUjgktG9t6aCzfkce')
