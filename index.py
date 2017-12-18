cf8e219520e0275fb8699cbd6830fc08f4eab65e
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-12-18 23:47:17 +0000

    Rejig

--- index.py
+++ index.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 import pprint, datetime, sched, time
+from random import *
 from algoliasearch import algoliasearch
 from parse import drives
 import xmljson
 pp = pprint.PrettyPrinter(indent=4)
 
+# Decides whether we're in testing mode or not
+Testing = False
+
+# Decide what to print out:
+toPrint = {
+  'cardsCreated': True
+}
+
 client = algoliasearch.Client('D3AE3TSULH', '1b36934cc0d93e04ef8f0d5f36ad7607') # This API key allows everything
 algoliaSourcesIndex = client.init_index('sources')
 
-def algoliaGetFilesIndex(organisationID):
+def algoliaGetFilesIndex(organisationID: str):
   algoliaFilesIndex = client.init_index(organisationID + '__Files')
   return algoliaFilesIndex
 
-def algoliaGetCardsIndex(organisationID):
+def algoliaGetCardsIndex(organisationID: str):
   algoliaCardsIndex = client.init_index(organisationID + '__Cards')
   return algoliaCardsIndex
 
-def addSource(data):
+def addSource(data: dict):
+  """This is for when a user adds a new source,
+  such as connecting up their Google Drive.
+  It automatically indexes all files after connecting.
+  """
+
   print('data:', data)
   source = {
     'objectID': data['source']['account']['id'],
@@ -25,15 +39,18 @@ def addSource(data):
   }
   print('source:', source)
   algoliaSourcesIndex.add_object(source)
+
   # Index all files from source
   accountInfo = {
     'organisationID': source['organisationID'],
     'accountID': source['objectID']
   }
+  time.sleep(5)
   indexFiles(accountInfo, False, True)
   return source
 
 def indexAll():
+  """Indexes all files since last memory update""" # Currently, all sources had the same Last Update Time stored
   accounts = drives.listAccounts()
   memory = open('memory.txt','r')
   lastRefreshTime = datetime.datetime.strptime(memory.read().splitlines()[0], '%Y-%m-%d %H:%M:%S.%f')
@@ -58,26 +75,33 @@ def indexAll():
   memory.write(thisRefreshTime)
 
 def indexFiles(accountInfo, after, allFiles=False):
+  """Indexes files from a query based on criteria given"""
   print(accountInfo)
   print(after)
   print(allFiles)
   if allFiles:
     print('Mytest 1')
-    files = drives.listfiles(accountInfo)
+    files = drives.listFiles(accountInfo)
   else:
-    files = drives.listfiles(accountInfo, after=after)
+    files = drives.listFiles(accountInfo, after=after)
   pp.pprint([f['title'] for f in files])
   pp.pprint(files)
 
   for f in files:
     indexFile(accountInfo, f['objectID']) # We index them again because they seem to first come in with their original names e.g. "Untitled document"
 
-def indexFile(accountInfo, fileID):
-  f = drives.getfile(accountInfo, fileID=fileID)
-  pp.pprint(f)
-  algoliaFilesIndex = algoliaGetFilesIndex(accountInfo['organisationID'])
-  algoliaFilesIndex.add_object(f)
-  indexFileContent(accountInfo, f)
+def indexFile(accountInfo, fileID: str):
+  f = drives.getFile(accountInfo, fileID=fileID)
+  if f is not None:
+    print('Not None')
+    # pp.pprint(f)
+    algoliaFilesIndex = algoliaGetFilesIndex(accountInfo['organisationID'])
+    algoliaFilesIndex.add_object(f)
+    if f['mimeType'] in ['application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
+      indexFileContent(accountInfo, f)
+    else:
+      if 'doc' in f['mimeType']:
+        print('"doc" was found in:', f['mimeType'])
 
 def indexFileContent(accountInfo, f):
   print(f)
@@ -86,33 +110,41 @@ def indexFileContent(accountInfo, f):
     'filters': 'fileID: "' + f['objectID'] + '"'
   }
   algoliaCardsIndex = algoliaGetCardsIndex(accountInfo['organisationID'])
-  algoliaCardsIndex.delete_by_query('', params)
+  algoliaCardsIndex.delete_by_query('', params) # Is this dangerous???
+
+  print('Deleted')
 
   # Create new chunks
   contentArray = drives.getContent(accountInfo, f['objectID'], True) # Should only take first one!!!
-  print('contentArray', contentArray)
   cards = createCardsFromContentArray(accountInfo, contentArray, f)['allCards']
-  # print(cards)
-  pp.pprint(cards)
+  print('Number of Cards:', len(cards))
+  if toPrint['cardsCreated']:
+    pp.pprint(cards)
 
 
-def createCardsFromContentArray(accountInfo, contentArray, f):
-  print('createCardsFromContentArray')
-  print('contentArray', contentArray)
+def createCardsFromContentArray(accountInfo, contentArray, f, parentContext=[]):
+  # print('createCardsFromContentArray')
+  # print('contentArray', contentArray)
   cards = []
   allCards = []
   for i, chunk in enumerate(contentArray):
     card = {
-      'content': ' '.join(chunk['content']),
+      'content': chunk['content'],
       'fileID': f['objectID'],
       'fileTitle': f['title'],
+      'context': parentContext,
       'created': f['created'],
       'modified': f['modified'],
       'type': 'p',
       'index': i
     }
+    if 'title' in chunk:
+      card['title'] = chunk['title']
+      if len(parentContext) > 1 and parentContext[0] == 'AGREED TERMS':
+        card['title'] = 'Clause ' + card['title'] # Hack for Clauses in Contract
     if 'chunks' in chunk:
-      subdata = createCardsFromContentArray(accountInfo, chunk['chunks'], f)
+      context = parentContext + [chunk['content']]
+      subdata = createCardsFromContentArray(accountInfo, chunk['chunks'], f, context)
       subcards = subdata['cards']
       allCards += subdata['allCards']
       card['listItems'] = [c['objectID'] for c in subcards]
@@ -122,10 +154,14 @@ def createCardsFromContentArray(accountInfo, contentArray, f):
   algoliaCardsIndex = algoliaGetCardsIndex(accountInfo['organisationID'])
   cardIDs = []
   try:
-    cardIDs = algoliaCardsIndex.add_objects(cards)
+    if Testing:
+      cardIDs = { 'objectIDs': [randint(1, 1000000000000) for c in cards] } # Use this when testing to avoid using up Algolia operations!!
+    else:
+      cardIDs = algoliaCardsIndex.add_objects(cards)
   except Exception as e:
-    print('Something went wrong saving this card to Algolia:,' + f['title'])
-  print(cardIDs['objectIDs'])
+    print('Something went wrong saving this card to Algolia:' + f['title'])
+    print(e)
+  # print(cardIDs['objectIDs'])
   for i, objectID in enumerate(cardIDs['objectIDs']):
     cards[i]['objectID'] = objectID
   return {
@@ -147,6 +183,9 @@ def startIndexing():
   s.run()
 
 
+
+"""Below here is stuff for testing"""
+
 # accountInfo = {
 #   'accountID': '284119499',
 #   'organisationID': 'tickbox'
@@ -159,12 +198,16 @@ def startIndexing():
 #   'accountID': '284151319'
 # }, allFiles=True, after=None)
 # indexFile({
+#   'organisationID': 'explaain',
+#   'accountID': '282782204'
+# }, 'FrgFloBV1hJZbWdgYDHIX9d6fL8IfaJ1lQzMiov53_HFhS7IKTeNrexpdc2xUbeYi')
+# indexFile({
 #   'organisationID': 'askporter',
 #   'accountID': '284151319'
 # }, 'FtORrzfQkKOM6NOR_ZgkDcBmP258Sne-HAMXW32x2F29Xr1VGyK2JKsqCq0eu704P')
 # indexFileContent({'objectID': 'FVNDMMXfVj99RqJMyz1xiFpk63kKA44NqCKEKimaUF1F63QxFJmvnRuuGKN2JyLXY', 'title': 'Policy Tracker for GE2017.com', 'modified': '1499418614', 'created': 'null'})
 
 
-# xmlstring = open('parse/sample.xml').read()
-# print(xmlstring)
+# xmlstring = open('parse/sample3.xml').read()
+# # print(xmlstring)
 # drives.xmlFindText(xmlstring)
--- parse/drives.py
+++ parse/drives.py
@@ -5,10 +5,22 @@ import xmljson
 from xmljson import parker, Parker
 # bf = BadgerFish(dict_type=OrderedDict)
 from xml.etree.ElementTree import fromstring
-pp = pprint.PrettyPrinter(indent=1)
+pp = pprint.PrettyPrinter(indent=1, width=160)
 
 import kloudless
 kloudless.configure(api_key='q4djKR_UXs8MxmUv8M56WdTVihDK6Z7ci8JnL1qJvC2Xx40T')
+
+# Decide what to print out:
+toPrint = {
+  'fileList': True,
+  'xmlString': False,
+  'elements': False,
+  'arrayOfPars': False,
+  'justScores': False,
+  'hierarchies': False,
+  'hierarchyText': True
+}
+
 
 def listAccounts():
   accounts = kloudless.Account.all()
@@ -20,78 +32,30 @@ def getAccount(accountID):
 
 
 
-def getContent(accountInfo, fileID, asArray=False):
-  try:
-    account = getAccount(accountInfo['accountID'])
-    kfile = account.files.retrieve(fileID)
-    content = kfile.contents().content
-    z = zipfile.ZipFile(io.BytesIO(content))
-    fileList = z.infolist()
-    pp.pprint(fileList)
-    docs = [f for f in fileList if f.filename == 'word/document.xml'] # or 'xl' in f.filename]
-    print(len(docs))
-    if len(docs):
-      xmls = []
-      for doc in docs:
-        xmlContent = z.read(doc).decode('utf-8')
-        pp.pprint('join(xmlContent)')
-        pp.pprint(''.join(xmlContent))
-        xmls.append(xmlFindText(xmlContent))
-        print('xml', xmls[-1])
-      return xmls[0]
-      # reg = re.findall('>([^<>]*)<', xmlContent)
-      # contentArray = [r for r in reg if len(r) > 0 and r != '\r']
-      # if asArray:
-      #   return contentArray
-      # else:
-      #   content = ' '.join(contentArray)
-      #   return content
-    else:
-      return ''
-    pass
-  except Exception as e:
-    print(e)
-    return ''
-
-def search(accountInfo, query):
+def listFiles(accountInfo, after=False, number=500):
   account = getAccount(accountInfo['accountID'])
-  print(query)
-  queries = query.split()[:8] # Max 8 words
-  split = lambda A, n=2: [' '.join(str(x) for x in A[i:i+n]) for i in range(0, len(A), n)]
-  queries = split(queries)
-  print('\n\nqueries\n', queries)
-  allResultsLists = list(map(lambda x: account.search.all(q=x), queries))
-  files = []
-  for resultsList in allResultsLists:
-    for result in resultsList:
-      if not any(f['id'] == result['id'] for f in files):
-        files.append(result)
-  pp.pprint('\n\nfiles:\n' + '\n'.join(list(map(lambda x: x['name'], files))))
-  # pp.pprint(list(map(lambda x: x['name'], files)))
-  # pp.pprint(files)
-  cards = list(map(fileToCard, files))
-  return cards
-
-def listfiles(accountInfo, after=False, number=500):
-  account = getAccount(accountInfo['accountID'])
-  print(after)
   recent = []
   if after:
     recent = account.recent.all(page_size=number, after=after)
   else:
-    print('Mytest 2')
     recent = account.recent.all(page_size=number)
-  pp.pprint(len(recent))
-  pp.pprint('files:' + '\n'.join(list(map(lambda x: x['name'], recent))))
+  print('Number of Files:', len(recent))
+  print('files: ', '\n'.join(list(map(lambda x: x['name'], recent))))
   files = [fileToAlgolia(f) for f in recent]
   return files
 
-def getfile(accountInfo, fileID):
+def getFile(accountInfo, fileID):
   account = getAccount(accountInfo['accountID'])
-  f = account.files.retrieve(id=fileID)
-  print('file:')
-  pp.pprint(f)
-  return fileToAlgolia(f)
+  f = {}
+  try:
+    f = account.files.retrieve(id=fileID)
+    f = fileToAlgolia(f)
+    print('file:')
+    pp.pprint(f)
+  except Exception as e:
+    f = None
+    print('Error getting file: "' + fileID + '"', e)
+  return f
 
 def fileToAlgolia(f):
   print(f)
@@ -121,58 +85,63 @@ def fileToCard(f):
   }
   return card
 
-# def xmlFindText(xmlContent):
-#   obj = json.loads(json.dumps(parker.data(fromstring(xmlContent))).replace('{http://schemas.openxmlformats.org/wordprocessingml/2006/main}',''))
-#   pp.pprint(obj)
-#   textArray = []
-#   ongoingCounter = []
-#   xmlSearchTextInChildren(obj, None, 0, textArray, ongoingCounter)
-#   print('textArray')
-#   pp.pprint(textArray)
-#
-#   # For comparison:
-#   reg = re.findall('>([^<>]*)<', xmlContent)
-#   # print([r for r in reg if len(r) > 0 and r != '\r'])
-
-# def xmlSearchTextInChildren(obj, parentKeyOrItem, level, arrayPassedDown, ongoingCounter):
-#   # print(level)
-#   try:
-#     for keyOrItem in obj:
-#       # print('level:', level, ', keyOrItem:', keyOrItem)
-#
-#       if keyOrItem == '$':
-#         ongoingCounter.append(len(ongoingCounter))
-#         print({level: obj[keyOrItem]})
-#         arrayPassedDown.append({level: obj[keyOrItem]})
-#       if parentKeyOrItem == 'p':
-#         arrayPassedDown = []
-#       nextObj = keyOrItem if isinstance(keyOrItem, dict) or isinstance(keyOrItem, list) else (obj[keyOrItem] if isinstance(obj, dict) else None)
-#       # print('nextObj', nextObj)
-#       if isinstance(nextObj, dict) or isinstance(nextObj, list):
-#         print(ongoingCounter, keyOrItem)
-#         xmlSearchTextInChildren(nextObj, keyOrItem, level + 1, arrayPassedDown, ongoingCounter)
-#       # else:
-#       #   print('stopped')
-#
-#       if parentKeyOrItem == 'p':
-#         textArray = arrayPassedDown
-#         print('textArray1')
-#         pp.pprint(textArray)
-#   except Exception as e:
-#     print(e)
-
+def getContent(accountInfo, fileID, asArray=False):
+  try:
+    account = getAccount(accountInfo['accountID'])
+    kfile = account.files.retrieve(fileID)
+    content = kfile.contents().content
+    z = zipfile.ZipFile(io.BytesIO(content))
+    fileList = z.infolist()
+    if toPrint['fileList']:
+      pp.pprint(fileList)
+    docs = [f for f in fileList if f.filename == 'word/document.xml'] # or f.filename == 'word/numbering.xml'] # or f.filename == 'word/styles.xml' or f.filename == 'word/setting.xml' or f.filename == 'word/theme1.xml' or f.filename == 'word/document.xml.rels'] # or 'xl' in f.filename]
+    print(len(docs))
+    xmls = []
+    for doc in docs:
+      xmlContent = z.read(doc).decode('utf-8')
+      if toPrint['xmlString']:
+        print('join(xmlContent)')
+        print(''.join(xmlContent))
+      xmls.append(xmlFindText(xmlContent))
+    if len(xmls):
+      return xmls[0]
+    else:
+      return ''
+    pass
+  except Exception as e:
+    print(e)
+    return ''
 
 def xmlFindText(xmlContent):
   xml = untangle.parse(xmlContent)
   textArray = []
   ongoingCounter = []
   xmlSearchTextInChildren(xml, 0, textArray, ongoingCounter, { 'parFormatting': False, 'hyperlink': False })
+  for t in textArray:
+    t['ranking'] = sum(t['allRankings'].values())
   textArray = [chunk for chunk in textArray if len(chunk['content'])]
-  print('textArray')
-  pp.pprint(textArray)
+  if toPrint['arrayOfPars']:
+    pp.pprint(textArray)
+  if toPrint['justScores']:
+    for t in textArray:
+      print(t['ranking'], t['content'])
   chunkHierarchy = getChunkHierarchy(textArray)
-  pp.pprint(chunkHierarchy)
+  chunkHierarchy = completeContent(chunkHierarchy)
+  if toPrint['hierarchies']:
+    pp.pprint(chunkHierarchy)
+  if toPrint['hierarchyText']:
+    printChunks(chunkHierarchy, 0)
   return chunkHierarchy
+
+def printChunks(chunks, layer):
+  for chunk in chunks:
+    content = ''.join(chunk['content'])
+    content = content[:80] + '...' if len(content) > 80 else content
+    if 'title' in chunk:
+      content = chunk['title'] + ': ' + content
+    print('    ' * layer, content)
+    if 'chunks' in chunk:
+      printChunks(chunk['chunks'], layer + 1)
 
 def xmlSearchTextInChildren(xml, level, dataPassedDown, ongoingCounter, context):
   children = xml.__dict__['children'] or xml['children']
@@ -181,23 +150,30 @@ def xmlSearchTextInChildren(xml, level, dataPassedDown, ongoingCounter, context)
     for child in children:
       child.context = dict(context) # Is NOT a deep copy
     for i, child in enumerate(children):
-      print(spacing, child)
+      if toPrint['elements']:
+        print(spacing, child)
       if child._name == 'w_p':
         chunkData = {
           'content': [],
-          'ranking': 0
+          'allRankings': {},
+          'otherContext': {}
         }
       else:
         chunkData = dataPassedDown
-
+      if child._name == 'w_tbl':
+        child.context['table'] = True
       if 'cdata' in child.__dict__ and child.__dict__['cdata'] and not child.__dict__['cdata'].isspace() and child._name != 'w_instrText':
         dataPassedDown['content'].append(child.__dict__['cdata'].strip())
         if child.context['hyperlink']:
           dataPassedDown['content'][-1] = '[' + dataPassedDown['content'][-1] + '](' + child.context['hyperlink'] + ')'
+      if child._name == 'w_numPr':
+        chunkData['otherContext']['numbered'] = True
       if child._name == 'w_pPr':
         child.context['parFormatting'] = True
       if child.context['parFormatting']:
-        dataPassedDown['ranking'] += getRanking(child)
+        ranking = getRanking(child)
+        if sum(ranking.values()) != 0:
+          dataPassedDown['allRankings'] = {**dataPassedDown['allRankings'], **ranking}
       if hasattr(child, 'w_instrText') and 'cdata' in child.w_instrText.__dict__ and 'HYPERLINK' in child.w_instrText.__dict__['cdata'] and i < len(children) - 1:
         p = re.compile('HYPERLINK \"(.*)\"')
         children[i+1].context['hyperlink'] = p.search(child.w_instrText.__dict__['cdata']).group(1)
@@ -206,58 +182,60 @@ def xmlSearchTextInChildren(xml, level, dataPassedDown, ongoingCounter, context)
       except Exception as e:
         print(e)
       if child._name == 'w_p':
+        for prop in ['w_sz']:
+          if prop not in chunkData['allRankings'] and len(dataPassedDown) and prop in dataPassedDown[-1]['allRankings']:
+            chunkData['allRankings'][prop] = dataPassedDown[-1]['allRankings'][prop]
         dataPassedDown.append(chunkData)
 
 
 def getRanking(el):
   ranking = 0
+  if 'table' in el.context:
+    ranking = -40 # Hack for the Contract doc
   if el._name == 'w_numPr':
     ranking = -10
+  if el._name == 'w_numId' and el._attributes['w:val'] == "8": # Purely a hack for Contract doc
+    ranking = 11
   if el._name == 'w_ilvl':
     ranking = -10 * int(el._attributes['w:val'])
-  if el._name == 'w_sz': # Doesn't appear to work on headings?
+  if el._name == 'w_ind':
+    ranking = -1 * int(el._attributes['w:left']) / 20
+  if el._name == 'w_sz':
     ranking = int(el._attributes['w:val'])
-  if el._name == 'w_pStyle':
-    print('w_pStyle w_pStyle')
-    style = el._attributes['w:val']
-    styles = {
-      'Heading1': 400,
-      'Heading2': 300,
-      'Heading3': 200,
-      'Heading4': 100,
-      'Heading5': 60,
-      'Heading6': 40
-    }
-    print('style', style)
-    if style in styles:
-      ranking = styles[style]
+  # if el._name == 'w_pStyle':
+  #   style = el._attributes['w:val']
+  #   styles = {
+  #     'Heading1': 400,
+  #     'Heading2': 300,
+  #     'Heading3': 200,
+  #     'Heading4': 100,
+  #     'Heading5': 60,
+  #     'Heading6': 40
+  #   }
+  #   if style in styles:
+  #     ranking = styles[style]
   if el._name == 'w_b' and el._attributes['w:val'] == '1':
     ranking = 10
-  return ranking
+  return {
+    el._name: ranking
+  }
 
 def getChunkHierarchy(textArray):
-  print('getChunkHierarchy')
-  print(textArray)
   chunks = []
   base = 0
   for i, chunk in enumerate(textArray):
-    print(chunk['ranking'])
     if chunk['ranking'] >= textArray[base]['ranking'] and i > 0:
-      print('go!!', base+1, i)
       if textArray[i-1]['ranking'] == chunk['ranking']:
         chunks.append(textArray[i-1])
       else:
-        print(textArray[base+1:i])
         myChunk = textArray[base]
         myChunk['chunks'] = getChunkHierarchy(textArray[base+1:i])
         chunks.append(myChunk)
       base = i
     if i == len(textArray)-1:
-      print('end!!', base+1, i)
       if textArray[i]['ranking'] == textArray[base]['ranking']:
         chunks.append(textArray[i])
       else:
-        print(textArray[base+1:])
         myChunk = textArray[base]
         myChunk['chunks'] = getChunkHierarchy(textArray[base+1:])
         chunks.append(myChunk)
@@ -267,7 +245,42 @@ def getChunkHierarchy(textArray):
   #   chunks = textArray
   return chunks
 
+# Joins content arrays and adds numbering
+def completeContent(chunks, parentNumber=False):
+  for i, chunk in enumerate(chunks):
+    chunk['content'] = ' '.join(chunk['content'])
+    number = parentNumber
+    if 'numbered' in chunk['otherContext']:
+      number = (str(number) + '.' if number else '') + str(i + 1)
+      chunk['title'] = number # + ' ' + chunk['content']
+    if 'chunks' in chunk:
+      chunk['chunks'] = completeContent(chunk['chunks'], number)
+  return chunks
 
+
+"""Below here is functions no longer used"""
+
+def search(accountInfo, query):
+  account = getAccount(accountInfo['accountID'])
+  print(query)
+  queries = query.split()[:8] # Max 8 words
+  split = lambda A, n=2: [' '.join(str(x) for x in A[i:i+n]) for i in range(0, len(A), n)]
+  queries = split(queries)
+  print('\n\nqueries\n', queries)
+  allResultsLists = list(map(lambda x: account.search.all(q=x), queries))
+  files = []
+  for resultsList in allResultsLists:
+    for result in resultsList:
+      if not any(f['id'] == result['id'] for f in files):
+        files.append(result)
+  pp.pprint('\n\nfiles:\n' + '\n'.join(list(map(lambda x: x['name'], files))))
+  # pp.pprint(list(map(lambda x: x['name'], files)))
+  # pp.pprint(files)
+  cards = list(map(fileToCard, files))
+  return cards
+
+
+"""Below here is stuff for testing"""
 
 # search('', '', 'savvy')
 # getContent('', '', 'FvsojaQrWWSNvuxbjS_hrw_fEVkEY8ykbb5kJh_cB-C_l-zw9ZnOEXZsq9HIp15dD')
